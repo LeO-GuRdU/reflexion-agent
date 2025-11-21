4aa36e9298c6d0013694643e1f24d6175d64a82f
leonardo-gurdulich <leonardo.gurdulich@halopowered.com>
2025-11-21 12:38:54 -0300

    Primer approach funcionando y devolviendo data

---
+++ chains.py
@@ -0,0 +1,76 @@
+import datetime
+from dotenv import load_dotenv
+from langchain_core.output_parsers.openai_tools import (
+    JsonOutputToolsParser,
+    PydanticToolsParser,
+)
+from langchain_core.messages import HumanMessage, BaseMessage, AIMessage
+from langchain_core.runnables import RunnableLambda
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.messages.utils import convert_to_messages as lc_convert_to_messages
+
+from schemas import AnswerQuestion
+
+load_dotenv()
+
+actor_prompt_template = ChatPromptTemplate.from_messages(
+    [
+        "system",
+        """You are expert researcher.
+        Current time: {time}
+
+        1. {first_instruction}
+        2. Reflect and critique your answer. Be severe to maximize improvement.
+        3. Recommend search queries to research information and improve your answer.""",
+        MessagesPlaceholder(variable_name="messages"),
+        ("system", "Answer the user's question above using the required format."),
+    ]
+).partial(
+    time=lambda: datetime.datetime.now().isoformat(),
+)
+
+llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash", temperature=0)
+parser = JsonOutputToolsParser(return_id=True)
+parser_pydantic = PydanticToolsParser(tools=[AnswerQuestion])
+
+actor_prompt_template = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """You are expert researcher.
+            Current time: {time}
+
+            1. {first_instruction}
+            2. Reflect and critique your answer. Be severe to maximize improvement.
+            3. Recommend search queries to research information and improve your answer.""",
+        ),
+        MessagesPlaceholder(variable_name="messages"),
+        ("system", "Answer the user's question above using the required format."),
+    ]
+).partial(
+    time=lambda: datetime.datetime.now().isoformat(),
+)
+
+first_responder_prompt_template = actor_prompt_template.partial(
+    first_instruction="Provide a detailed ~250 word answer to the question.",
+)
+
+first_responder = first_responder_prompt_template | llm.bind_tools(
+    tools=[AnswerQuestion], tool_choice="AnswerQuestion"
+)
+
+if __name__ == "__main__":
+    human_message = HumanMessage(
+        content="Escribe un doc que elabore una dieta balanceada para una mujer en los primeros 3 meses de embarazo"
+        "listando los alimentos recomendados y los que se deben evitar."
+    )
+
+    chain = (
+        first_responder_prompt_template
+        | llm.bind_tools(tools=[AnswerQuestion], tool_choice="AnswerQuestion")
+        | parser_pydantic
+    )
+
+    res = chain.invoke(input={"messages": [human_message]})
+    print(res)
